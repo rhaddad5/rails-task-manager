86f2826f0148028bdd31a9073f9088edb1d1ef7d
Roxane Haddad <roxane.haddad@gmail.com>
2020-02-11 12:55:21 +0100

    finished tha task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,21 @@ class TasksController < ApplicationController
     redirect_to task_path(@new_task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
