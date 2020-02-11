d717265f772305e50b3a1880e7ad3dc836aee895
Roxane Haddad <roxane.haddad@gmail.com>
2020-02-11 12:32:17 +0100

    Added the creation of new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @new_task = Task.new
+  end
+
+  def create
+    @new_task = Task.new(task_params)
+    @new_task.save
+    redirect_to task_path(@new_task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
